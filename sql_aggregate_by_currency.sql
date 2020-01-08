fb3d6ecbedad6406410f2b69cba982460b4209da
Neo Ahn <ahynest@naver.com>
2020-01-08 16:18:17 +0900

    Update sql_aggregate_by_currency.sql

--- sql_aggregate_by_currency.sql
+++ sql_aggregate_by_currency.sql
@@ -2,46 +2,46 @@
 
 -- 1-1) 집계합수에 서브쿼리로 환율을 반환하여 달러화 화폐로 계산
 SELECT 	a.bs_gb, a.user_nm, COUNT(a.bs_gb), SUM(a.bs_amt_krw),
-			ROUND(SUM(a.bs_amt * 	(CASE a.currency_un
-												WHEN 'USD' THEN 1
-												ELSE (SELECT b.fx_rate_usd FROM co_account.currencies b WHERE b.currency_un = a.currency_un)
-											END)), 2) AS bs_amt_usd
- FROM v_budget_settlement a
- WHERE a.suspended = 0
- GROUP BY a.bs_gb, a.user_nm WITH ROLLUP;
+		ROUND(SUM(a.bs_amt * 	(CASE a.currency_un
+									WHEN 'USD' THEN 1
+									ELSE (SELECT b.fx_rate_usd FROM co_account.currencies b WHERE b.currency_un = a.currency_un)
+							     END)), 2) AS bs_amt_usd
+    FROM v_budget_settlement a
+    WHERE a.suspended = 0
+    GROUP BY a.bs_gb, a.user_nm WITH ROLLUP;
 
 -- 1-2) 집계합수에 서브쿼리로 환율을 반환하여 원화 화폐로 계산
 SELECT 	a.bs_gb, a.user_nm, COUNT(a.bs_gb), SUM(a.bs_amt_krw),
-			ROUND(SUM(a.bs_amt * 	(CASE a.currency_un
-												WHEN 'KRW' THEN 1
-												ELSE (SELECT b.fx_rate_krw FROM co_account.currencies b WHERE b.currency_un = a.currency_un)
-											END)), 2) AS bs_amt_usd
- FROM v_budget_settlement a
- WHERE a.suspended = 0
- GROUP BY a.bs_gb, a.user_nm WITH ROLLUP;
+		ROUND(SUM(a.bs_amt * 	(CASE a.currency_un
+									WHEN 'KRW' THEN 1
+									ELSE (SELECT b.fx_rate_krw FROM co_account.currencies b WHERE b.currency_un = a.currency_un)
+								END)), 2) AS bs_amt_usd
+    FROM v_budget_settlement a
+    WHERE a.suspended = 0
+    GROUP BY a.bs_gb, a.user_nm WITH ROLLUP;
  
 -- 1-3) 집계합수에 서브쿼리로 환율을 반환하여 유로화 화폐로 계산
 SELECT 	a.bs_gb, a.user_nm, COUNT(a.bs_gb), SUM(a.bs_amt_krw),
-			ROUND(SUM(a.bs_amt * 	(CASE a.currency_un
-												WHEN 'EUR' THEN 1
-												ELSE (SELECT b.fx_rate_usd FROM co_account.currencies b WHERE b.currency_un = a.currency_un) /
-													  (SELECT c.fx_rate_usd FROM co_account.currencies c WHERE c.currency_un = 'EUR')
-											END)), 2) AS bs_amt_usd
- FROM v_budget_settlement a
- WHERE a.suspended = 0
- GROUP BY a.bs_gb, a.user_nm WITH ROLLUP;
+		ROUND(SUM(a.bs_amt * 	(CASE a.currency_un
+									WHEN 'EUR' THEN 1
+									ELSE (SELECT b.fx_rate_usd FROM co_account.currencies b WHERE b.currency_un = a.currency_un) /
+									     (SELECT c.fx_rate_usd FROM co_account.currencies c WHERE c.currency_un = 'EUR')
+								END)), 2) AS bs_amt_usd
+    FROM v_budget_settlement a
+    WHERE a.suspended = 0
+    GROUP BY a.bs_gb, a.user_nm WITH ROLLUP;
 
  
 -- 2) 공통테이블표현식으로 달러화 환율을 적용한 임시 테이블 생성 후 집계
 WITH cte_budget_settlement AS(
 SELECT 	a.bs_gb, a.user_nm, a.bs_amt_krw, a.currency_un, a.bs_amt,
-			ROUND((CASE a.currency_un
-						WHEN 'USD' THEN a.bs_amt
-						ELSE a.bs_amt * (SELECT b.fx_rate_usd FROM co_account.currencies b WHERE b.currency_un = a.currency_un)
-					END), 2) AS bs_amt_usd
+		ROUND((CASE a.currency_un
+	       		WHEN 'USD' THEN a.bs_amt
+				ELSE a.bs_amt * (SELECT b.fx_rate_usd FROM co_account.currencies b WHERE b.currency_un = a.currency_un)
+			END), 2) AS bs_amt_usd
 	FROM financial_db.v_budget_settlement a
 	WHERE a.suspended = 0
 )
 SELECT bs.bs_gb, bs.user_nm, COUNT(bs.bs_gb), SUM(bs_amt_krw), SUM(bs_amt_usd) 
-	FROM cte_budget_settlement bs 
+    FROM cte_budget_settlement bs 
 	GROUP BY bs.bs_gb, bs.user_nm;
